6bfdaf16ed20f1cf9cf3b8b4ad05bda367acb3fd
Kerry Buckley <kerryjbuckley@gmail.com>
2013-03-19 13:58:04 +0000

    Skeleton Rails app

---
+++ Gemfile
@@ -0,0 +1,9 @@
+source "https://rubygems.org"
+
+gem "rails"
+gem "sass-rails"
+gem "coffee-rails"
+gem "haml"
+gem "jquery-rails"
+gem "therubyracer"
+gem "uglifier"
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Demo::Application.config.secret_token = '78cdf904844ba3def7e0a1e7b0db09e2476f18a55e4308b1269246b305dba7825a8e1bd2e9128763766569a28f853782c2d1b3512a48430d8cb765dc34c1ae0f'
